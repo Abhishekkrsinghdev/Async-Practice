3273e3854d014fd5cb20a6d08133667121bf2d99
Abhishekkrsinghdev <hrithiksinghrocks31@gmail.com>
2023-07-09 22:47:10 +0530

    nested catch block

--- async.js
+++ async.js
@@ -1,23 +1,27 @@
-function demo(){
-    // return new Promise((resolve,reject)=>{
-    //     //resolve(10);
-    //     //reject(20);
-    // })
-    return Promise.reject(10);
+function demo() {
+  // return new Promise((resolve,reject)=>{
+  //     //resolve(10);
+  //     //reject(20);
+  // })
+  return Promise.reject(10);
 }
 
-async function consume(){
+async function consume() {
+  try {
     try {
-        const value=await demo();
-        console.log("Inside try------- ",value);
-        return 5;
-        
+      const value = await demo();
+      console.log("Inside try------- ", value);
+      return 5;
     } catch (errorValue) {
-       console.log("Inside error-----------",errorValue); 
-       throw {err:"Throwing error"};
+      console.log("Inside error-----------", errorValue);
+      throw { err: "Throwing error" };
     }
+  } catch (errorValue) {
+    console.log("catching error of cathhhhhhh", errorValue);
+  }
 }
-/**Here we can also put the consume funcn call inside another async funcn to handle its errors as follow 
+consume();
+/**Here we can also put the consume funcn call inside another async funcn to handle its errors as follow
  * async function consume1(){
  *      try{
  *          let res=await consume();
@@ -25,19 +29,23 @@ async function consume(){
  *       catch(error){
  *          console.log("error")
  *      }
- * 
+ *
  * }
- * 
- * 
-*/
-
+ *
+ *
+ */
 
 /**Here I have mixed both async await as well as promise the above consume is using async await its error
  * is using promise
  * */
 
-consume().then((value)=>{
-    console.log("successful execution-----",value);
-},(errorValue)=>{
-    console.log("errrrrrrrrrrr",errorValue);
-});
+// consume().then((value)=>{
+//     console.log("successful execution-----",value);
+// },(errorValue)=>{
+//     console.log("errrrrrrrrrrr",errorValue);
+// });
+
+/**
+ * 
+ * Here .catch syntax can also be used 
+ */
